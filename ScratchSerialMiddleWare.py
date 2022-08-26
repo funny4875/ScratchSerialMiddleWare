c0593b5c1c7f9741b1b45252b6c0b81400b9d33e
funny4875 <funny4875@gmail.com>
2022-08-26 21:28:57 +0800

    Add files via upload

--- ScratchSerialMiddleWare.py
+++ ScratchSerialMiddleWare.py
@@ -2,25 +2,11 @@ from scratchclient import ScratchSession
 import time
 import serial
 import sys
-
 username = "ccsh_ky" 
 password = "123456987" 
 projectID = 724014896
 COMport = "COM6"
-inputVARs='A0_R,D4_R'
-import sys
-
-if len(sys.argv)>5:
-    username = sys.argv[1]
-    password = sys.argv[2]
-    projectID= sys.argv[3]
-    COMport  = sys.argv[4]
-    inputVARs = sys.argv[5]
-else:
-    print('依序附帶下列5個參數，以空白字元當間隔\n[1]username [2]password [3]projectID [4]COMport [5]inputVARs\nanykey...')
-    input()
-    sys.exit()
-
+inputVARs='A0_R,D4_R,A1_R'
 try:
     serialPort = serial.Serial(port = COMport, baudrate=9600,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
@@ -59,17 +45,22 @@ while True:
     
     if(serialPort.in_waiting > 0):
         serialString = serialPort.read(size=5)
-       
-        if serialString[0]!= lastSerialString[0] and 'A0_R' in INPUT_VAR:
-                connection.set_cloud_variable('A0_R', serialString[0])
-        if serialString[1]!= lastSerialString[1] and 'A1_R' in INPUT_VAR:
-                connection.set_cloud_variable('A1_R', serialString[1])
-        if serialString[2]!= lastSerialString[2] and 'A2_R' in INPUT_VAR:
-                connection.set_cloud_variable('A2_R', serialString[2])
-        if serialString[3]!= lastSerialString[3] and 'A3_R' in INPUT_VAR:
-                connection.set_cloud_variable('A3_R', serialString[3])
-        if serialString[4]!= lastSerialString[4] and 'D4_R' in INPUT_VAR:
-                connection.set_cloud_variable('D4_R', serialString[4])
+        try:
+            if serialString[0]!= lastSerialString[0] and 'A0_R' in INPUT_VAR:
+                    connection.set_cloud_variable('A0_R', serialString[0])
+            if serialString[1]!= lastSerialString[1] and 'A1_R' in INPUT_VAR:
+                    connection.set_cloud_variable('A1_R', serialString[1])
+            if serialString[2]!= lastSerialString[2] and 'A2_R' in INPUT_VAR:
+                    connection.set_cloud_variable('A2_R', serialString[2])
+            if serialString[3]!= lastSerialString[3] and 'A3_R' in INPUT_VAR:
+                    connection.set_cloud_variable('A3_R', serialString[3])
+            if serialString[4]!= lastSerialString[4] and 'D4_R' in INPUT_VAR:
+                    connection.set_cloud_variable('D4_R', serialString[4])
+        except:
+            print('error while set_cloud_variable()')
+            session.close()
+            session = ScratchSession(username, password)
+            connection = session.create_cloud_connection(projectID)
         lastSerialString = serialString
         
     serialPort.flush()
